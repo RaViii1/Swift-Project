8a98311ff756e2c51d02d80ad6834951f8e53723
RaVi <77927691+RaViii1@users.noreply.github.com>
2023-06-06 09:40:01 +0200

    Update LoginView.swift

--- projekt/projekt/LoginView.swift
+++ projekt/projekt/LoginView.swift
@@ -6,35 +6,48 @@
 //
 
 import SwiftUI
-
 struct LoginView: View {
     @State private var username = ""
     @State private var password = ""
+    @State private var isPresentingRegisterView = false
 
     var body: some View {
-        VStack() {
-            Text("Login")
-                .font(.system(size: 48))
-               
-            TextField("Username", text: $username)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-
-            Button(action: {
-                // Perform login action here
-            }) {
+        NavigationView { // Embed everything in a navigation view
+            VStack() {
                 Text("Login")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity, minHeight: 44)
-                    .background(Color.blue)
-                    .cornerRadius(8.0)
+                    .font(.system(size: 48))
+                   
+                TextField("Username", text: $username)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                Button(action: {
+                    // Perform login action here
+                }) {
+                    Text("Login")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, minHeight: 44)
+                        .background(Color.blue)
+                        .cornerRadius(8.0)
+
+                 }
+                
+                 // Replace NavigationLink with Button
+                 Button(action: { self.isPresentingRegisterView.toggle() }) {
+                     Text("Don't have an account? Register here.")
+                         .foregroundColor(Color.blue)
+                  }
             }
-        }
-        .padding()
-    }
+
+           .fullScreenCover(isPresented:$isPresentingRegisterView){
+               RegisterView()
+           }
+            .padding()
+       }
+   }
 }
 
 struct ThirdView_Previews: PreviewProvider {
