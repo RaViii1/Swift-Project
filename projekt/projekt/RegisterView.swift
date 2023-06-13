8b99fe116b07fff296d6bb7d1d992ad7590985b8
damymeister <77884237+damymeister@users.noreply.github.com>
2023-06-13 08:26:52 +0200

    Update RegisterView.swift

--- projekt/projekt/RegisterView.swift
+++ projekt/projekt/RegisterView.swift
@@ -8,23 +8,59 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @Environment(\.presentationMode) var presentationMode
     @State private var username = ""
     @State private var password = ""
-    @State private var isPresentingLoginView = false
+    @State private var usernameValidate = ""
+    @State private var passwordValidate = ""
+    private var isUsernameValid: Bool {
+        return username.count >= 6
+    }
+    private var isPasswordValid: Bool {
+        let passwordRegex = "(?=.*[A-Z])(?=.*[0-9]).{7,}"
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        return passwordPredicate.evaluate(with: password)
+    }
 
     var body: some View {
-        VStack() {
+        VStack {
+            Spacer()
             Text("Register")
                 .font(.system(size: 48))
-               
+           
+            
             TextField("Username", text: $username)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+                .padding()
+            
+            if usernameValidate != "" {
+                Text(usernameValidate)
+                    .foregroundColor(.red)
+            }
             
             SecureField("Password", text: $password)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            if passwordValidate != "" {
+                Text(passwordValidate)
+                    .foregroundColor(.red)
+            }
 
             Button(action: {
-                // Perform registration action here
+                if isUsernameValid && isPasswordValid {
+                    usernameValidate = ""
+                    passwordValidate = ""
+                    
+                } else if !isUsernameValid {
+                    usernameValidate = "Your Username must include at least 6 characters."
+                }
+                else if !isPasswordValid {
+                    passwordValidate = "Your password must include at least 7 characters, one digit and one capital letter."
+                }
+        
             }) {
                 Text("Register")
                     .font(.headline)
@@ -32,20 +68,21 @@ struct RegisterView: View {
                     .frame(maxWidth: .infinity, minHeight: 44)
                     .background(Color.green)
                     .cornerRadius(8.0)
+                    .padding()
             }
+
+            Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Back")
+                    .foregroundColor(Color.blue)
+            }
+            .padding()
             
-             // Replace NavigationLink with Button
-             Button(action:{self.isPresentingLoginView.toggle()}){
-                 Text("Do you have an account? Login here.")
-                     .foregroundColor(Color.blue)
-              }
-             
-         }
-           .fullScreenCover(isPresented:$isPresentingLoginView){
-               LoginView()
-           }
-         .padding()
-     }
+            Spacer()
+        }
+        .padding()
+    }
 }
 
 struct RegisterView_Previews: PreviewProvider {
